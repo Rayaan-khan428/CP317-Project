85096fcf419685859dca3237accf2118213f5f60
Ryan Mood <127367763+ryanmood@users.noreply.github.com>
2023-07-19 23:29:58 -0400

    Add files via upload
    Working (sorta) split is still not splitting based on numbers, letters or roman numerals :(

--- PDFDivision.java
+++ PDFDivision.java
@@ -6,43 +6,100 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-// Divides into paragraphs (which is acc lines lmfao) for now
-// Will change 
-
 public class PDFDivision {
 
+	// Word min to stop it from splitting small useless paragraphs (on zero for testing but probs gonna be like 50?) 
+	// Still need to work out regex stuff to make the word limit only apply to paragraphs split 
+	// Rn it only checks the first line for regex 
+	
+	// Mabye do the opposite and split the paragprahs based on regex then split again as needed if they are too large? 
+	private static final int MIN_WORDS_PER_SECTION = 0;
+
     public static void divide(String inputFileLocation) {
-        // Input file path from the main function
         String inputFilePath = inputFileLocation + "\\result.txt";
-//        System.out.println(inputFileLocation);
-//        System.out.println(inputFilePath);
+        int sectionCount = 1;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
-            // Read the text from the input file
             StringBuilder text = new StringBuilder();
             String line;
             while ((line = reader.readLine()) != null) {
                 text.append(line).append("\n");
             }
 
-            // Split the text based on empty lines (paragraphs)
-            String[] paragraphs = text.toString().split("\\s*\\n\\s*");
+            // Check if the entire text contains the regex pattern
+            boolean containsRegexPattern = text.toString().matches("(?s).*[IVXLCDM]+|[A-Z]+\\b|[0-9]+\\b.*");
 
-            // Save each paragraph into separate files
-            for (int i = 0; i < paragraphs.length; i++) {
-                String paragraph = paragraphs[i];
+            if (containsRegexPattern) {
+                // Find all sections using the regex pattern
+                String[] sections = text.toString().split("[IVXLCDM]+|[A-Z]+\\b|[0-9]+\\b");
 
-                // Skip empty paragraphs
-                if (paragraph.trim().isEmpty()) {
-                    continue;
+                
+                // Save each section into separate files
+                for (int i = 0; i < sections.length; i++) {
+                    String section = sections[i].trim();
+
+                    // Skip empty sections
+                    if (section.isEmpty()) {
+                        continue;
+                    }
+
+                    // Save section into "sectionX.txt", where X is the section number (starting from 1)
+                    String outputFilePath = inputFileLocation + "\\section" + (sectionCount) + ".txt";
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+                        writer.write(section);
+                        sectionCount += 1;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                // If no sections are identified, split the text into paragraphs
+                String[] paragraphs = text.toString().split("(?m)^\\s*$");
+
+                StringBuilder sectionBuilder = new StringBuilder();
+                int wordCount = 0;
+
+                for (int i = 0; i < paragraphs.length; i++) {
+                    String paragraph = paragraphs[i].trim();
+
+                    // Skip empty paragraphs
+                    if (paragraph.isEmpty()) {
+                        continue;
+                    }
+
+                    // Count the words in the current paragraph
+                    int paragraphWordCount = paragraph.split("\\s+").length;
+                    wordCount += paragraphWordCount;
+
+                    // Append the current paragraph to the sectionBuilder
+                    sectionBuilder.append(paragraph).append("\n");
+
+                    // Check if the word count in the sectionBuilder meets the minimum requirement
+                    if (wordCount >= MIN_WORDS_PER_SECTION) {
+                        // Save the section into "sectionX.txt", where X is the section number (starting from 1)
+                        String outputFilePath = inputFileLocation + "\\section" + (sectionCount) + ".txt";
+                        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+                            writer.write(sectionBuilder.toString());
+                            sectionCount += 1;
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        // Reset sectionBuilder and wordCount for the next section
+                        sectionBuilder.setLength(0);
+                        wordCount = 0;
+                    }
                 }
 
-                // Save paragraph into "sectionX.txt", where X is the paragraph number (starting from 1)
-                String outputFilePath = inputFileLocation + "\\section" + (i) + ".txt";
-                try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
-                    writer.write(paragraph);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                // Save the remaining text in sectionBuilder as the last section
+                if (sectionBuilder.length() > 0) {
+                    String outputFilePath = inputFileLocation + "\\section" + sectionCount + ".txt";
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+                        writer.write(sectionBuilder.toString());
+                        sectionCount += 1;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
 
