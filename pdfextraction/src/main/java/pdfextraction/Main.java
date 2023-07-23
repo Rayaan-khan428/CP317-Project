58bd6efcbfd8a73bcf5efd3da7e14d9fd01e386c
Sebastian Balderrama <sebastian.balderr@gmail.com>
2023-07-23 17:18:07 -0400

    Update Main.java

--- pdfextraction/src/main/java/pdfextraction/Main.java
+++ pdfextraction/src/main/java/pdfextraction/Main.java
@@ -21,15 +21,20 @@ public class Main {
         
         PDFExtract pdfExtractor = new PDFExtract(outputFolder);
         PDDocument document = PDDocument.load(new File(inputFileName));
-        
+         
         //System.out.println(outputFolder);
+        
 		// The methods. You can test each one separately if you want
         pdfExtractor.SaveImagesInPdf(document);
+        pdfExtractor.getNumImages();
+        pdfExtractor.getPage(1);
         pdfExtractor.ExtractText(document);
         pdfExtractor.onlyText(document);
+        
         // Reference to the folder is passed
         // Needs Ryan's PDFDivision.java to be imported work
         //PDFDivision.divide(outputFolder);
+        
         document.close();
     }
 }
