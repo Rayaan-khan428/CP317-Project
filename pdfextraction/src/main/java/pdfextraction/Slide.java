20c797511800b449ea995e56b33094541574106f
Erin Manson <126124271+ErinManson@users.noreply.github.com>
2023-07-25 19:51:01 -0400

    Update Slide.java

--- pdfextraction/src/main/java/pdfextraction/Slide.java
+++ pdfextraction/src/main/java/pdfextraction/Slide.java
@@ -3,12 +3,17 @@ package pdfextraction;
 public class Slide {
 	
 	public int SlideNum;
+	public int PageNum;
 	public String Title; 
+	public String Image;
+
 	
 	// Constructor 
-	public Slide(int SlideNum, String Title) {
+	public Slide(int SlideNum, int PageNum, String Title, String Image) {
 		this.SlideNum = SlideNum;
+		this.PageNum = PageNum;
 		this.Title = Title;
+		this.Image = Image;
 	}
 	
 	// Setters and Getters my fav :)
@@ -17,15 +22,32 @@ public class Slide {
 		this.Title = Title;
 	}
 	
+	public void setImage(String Image) {
+		this.Image = Image;
+	}
+	
 	public void setSlideNum(int SlideNum) {
 		this.SlideNum = SlideNum;
+	}
+
+	public void setPageNum(int PageNum) {
+		this.PageNum = PageNum;
 	}
 	
 	public String getTitle() {
 		return this.Title;
 	}
 	
+	public String getImage() {
+		return this.Image;
+	}
+	
 	public int getSlideNum() {
 		return this.SlideNum;
 	}
+	
+	public int getPageNum() {
+		return this.PageNum;
+	}
+	
 }
