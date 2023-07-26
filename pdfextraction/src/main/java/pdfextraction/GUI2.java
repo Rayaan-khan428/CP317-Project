210799574269752c2269e13ca6dbf61c7b2633f4
Sebastian Balderrama <sebastian.balderr@gmail.com>
2023-07-26 17:11:13 -0400

    Update GUI2.java

--- pdfextraction/src/main/java/pdfextraction/GUI2.java
+++ pdfextraction/src/main/java/pdfextraction/GUI2.java
@@ -1,95 +1,77 @@
 package pdfextraction;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 
+import javax.swing.JPanel;
+//import javax.swing.SwingConstants;
+//import javax.swing.SwingUtilities;
+import javax.swing.border.Border;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
+import javax.swing.JFrame;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.Border;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.google.gson.JsonIOException;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.JsonSyntaxException;
-
-public class GUI implements ActionListener{
-	
+public class GUI2 implements ActionListener{
 	final JFileChooser fc = new JFileChooser();
-	JButton selectButton = new JButton("Select a file");
+	private JPanel cardPanel;
+    private CardLayout cardLayout;
+    
+    JFrame frame = new JFrame("PDF-to-PPT");
+    
+    JButton selectButton = new JButton("Select a file");
 	JButton generateButton = new JButton("Generate PPT");
 	JButton downloadButton = new JButton("Download PPT");
-	
-	JLabel title = new JLabel("PDF-to-PPT");
-	JFrame frame = new JFrame();
-	JLabel fileLabel = new JLabel("No File Selected");
-	
-	JPanel panelTitle = new JPanel();
+    JButton switchToScreen1 = new JButton("Back");
+    JButton switchToScreen2 = new JButton("Next");
+    
+    JLabel fileLabel = new JLabel("No File Selected");
+    
 	JPanel panelBody = new JPanel();
 	JPanel panelBodyLower = new JPanel();
+	JPanel screen1 = createMainScreen("PDF-to-PPT");
+    JPanel screen2 = createScreen("PDF-to-PPT");
 	
-	ArrayList<Slide> presentation = new ArrayList<>();
-	String projectRoot = System.getProperty("user.dir");
-    PDDocument document = null;
-	
-	public GUI() {
-		Border borderCommon = BorderFactory.createEmptyBorder(0, 10, 10, 10);
-		Border borderBodyLower = BorderFactory.createEmptyBorder(70, 10, 10, 10);
+    Border borderCommon = BorderFactory.createEmptyBorder(0, 10, 10, 10);
+    Border borderBodyLower = BorderFactory.createEmptyBorder(70, 10, 10, 10);
+    
+    Color lightGray = Color.decode("#F2F2F2");
+    Color buttonColor = Color.decode("#20C997");
+    Color textColor = Color.WHITE;
+    
+    public GUI2() {
+    	fc.addChoosableFileFilter(new FileNameExtensionFilter("PDF Documents", "pdf"));
+    	
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        cardPanel = new JPanel();
+        cardLayout = new CardLayout();
+        cardPanel.setLayout(cardLayout);
+
+
+        // Add the screens to the cardPanel
+        cardPanel.add(screen1, "SCREEN1");
+        cardPanel.add(screen2, "SCREEN2");
+        
+        // Set up main buttons
 		
-		BoxLayout layoutTitle = new BoxLayout(panelTitle, BoxLayout.PAGE_AXIS);
-		BoxLayout layoutBody = new BoxLayout(panelBody, BoxLayout.PAGE_AXIS);
 		BoxLayout layoutBodyLower = new BoxLayout(panelBodyLower, BoxLayout.PAGE_AXIS);
-		
-		//Change title font and stuff here
-		title.setFont(new Font("Dialog",Font.PLAIN,72));
-		title.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-		
-		selectButton.addActionListener(this);
-		selectButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
-		
-		fileLabel.setAlignmentX(JButton.CENTER_ALIGNMENT);
-		
-		generateButton.addActionListener(this);
-		generateButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
 		
 		downloadButton.addActionListener(this);
 		downloadButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
-		
-		panelTitle.setBorder(borderCommon);
-		panelTitle.setLayout(layoutTitle);
-		panelTitle.add(title);
-		
-        panelBody.setLayout(layoutBody);
-        panelBody.setBorder(borderCommon);
-        panelBody.add(selectButton);
-        panelBody.add(fileLabel);
-        
+		      
         panelBodyLower.setLayout(layoutBodyLower);
         panelBodyLower.setBorder(borderBodyLower);
 		
-        //--------------------COLOUR STUFF---------------
-        Color lightGray = Color.decode("#F2F2F2");
-        Color buttonColor = Color.decode("#20C997");
-        Color textColor = Color.WHITE;
-        Color titleTextColor = Color.decode("#333333");
-
         frame.getContentPane().setBackground(lightGray);
         selectButton.setBackground(buttonColor);
         selectButton.setForeground(textColor);
@@ -97,26 +79,90 @@ public class GUI implements ActionListener{
         generateButton.setForeground(textColor);
         downloadButton.setBackground(buttonColor);
         downloadButton.setForeground(textColor);
-        title.setForeground(titleTextColor);
-        //------------------------------------------------
         
-		frame.add(panelTitle, BorderLayout.NORTH);
-		frame.add(panelBody);
+        
+		//-------------------------------------------------------------------------
 		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//To open the GUI at the center of the screen
-		frame.setLocationRelativeTo(null);
-		frame.setTitle("PDF-to-PPT");
-		frame.pack();
-		frame.setVisible(true);
+        // Set up buttons to switch between screens
+        switchToScreen1.addActionListener(this);
+        switchToScreen2.addActionListener(this);
+        
+        frame.getContentPane().setBackground(lightGray);
+        switchToScreen1.setBackground(buttonColor);
+        switchToScreen1.setForeground(textColor);
+        switchToScreen2.setBackground(buttonColor);
+        switchToScreen2.setForeground(textColor);        
+        
+        // Add buttons to a panel
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(switchToScreen1);
+        buttonPanel.add(switchToScreen2);
+        
+
+        // Add the buttonPanel to the SOUTH position
+        frame.add(cardPanel, BorderLayout.NORTH);
+        frame.add(Box.createRigidArea(new Dimension(0, 25)), BorderLayout.CENTER); 
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null); // Center the frame on the screen
+        frame.setVisible(true);
+    }
+    private JPanel createMainScreen(String title) {
+    	BoxLayout layoutBody = new BoxLayout(panelBody, BoxLayout.PAGE_AXIS);
+        
+        Color titleTextColor = Color.decode("#333333");
+        
+        JLabel titleLabel = new JLabel(title);
+        titleLabel.setFont(new Font("Dialog",Font.PLAIN,72));
+		titleLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        titleLabel.setForeground(titleTextColor);
+    	
+    	selectButton.addActionListener(this);
+		selectButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
 		
-	}
-	
-	public static void main(String[] args) {
-		new GUI();
+		fileLabel.setAlignmentX(JButton.CENTER_ALIGNMENT);
+		
+		generateButton.addActionListener(this);
+		generateButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+    	
+    	panelBody.setLayout(layoutBody);
+        panelBody.setBorder(borderCommon);
+        panelBody.add(titleLabel, BorderLayout.NORTH);
+        panelBody.add(selectButton);
+        panelBody.add(fileLabel);
+        
+    	return panelBody;
+    }
+    // Helper method to create a screen panel 
+    private JPanel createScreen(String title) {
+        JPanel panel = new JPanel();
+        
+        BoxLayout layoutTitle = new BoxLayout(panel, BoxLayout.PAGE_AXIS);
+        panel.setLayout(layoutTitle);
+        
+        panel.setBorder(borderCommon);
+        
+        Color titleTextColor = Color.decode("#333333");
+        
+        JLabel titleLabel = new JLabel(title);
+        titleLabel.setFont(new Font("Dialog",Font.PLAIN,72));
+		titleLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        titleLabel.setForeground(titleTextColor);
+        
+        generateButton.addActionListener(this);
+		generateButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        
+        panel.add(titleLabel, BorderLayout.CENTER);
+
+        return panel;
     }
 
-	@Override
+    public static void main(String[] args) {
+    	new GUI2();
+    }
+    
+    @Override
 	public void actionPerformed(ActionEvent e) {
 	    if (e.getSource() == selectButton) {	    	
 	    	//----------------FILE NAME/PATH STUFF---------------------
@@ -126,15 +172,37 @@ public class GUI implements ActionListener{
 	            File file = fc.getSelectedFile();
 	            //String filePath = file.getAbsolutePath();
 	            
+	            Icon fileIcon = FileSystemView.getFileSystemView().getSystemIcon(file);
+	            
+	            //-----------------------Terrible Code, run if you want lol------------------------------------
+//	            int newWidth = fileIcon.getIconWidth() * 10;
+//	            int newHeight = fileIcon.getIconHeight() * 10;
+//
+//	            // Get the Image object from the ImageIcon
+//	            Image originalImage = ((ImageIcon)fileIcon).getImage();
+//
+//	            // Use Image#getScaledInstance() to resize the Image
+//	            Image resizedImage = originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_DEFAULT);
+//
+//	            // Create a new ImageIcon with the resized Image
+//	            ImageIcon resizedIcon = new ImageIcon(resizedImage);
+	            //------------------------------------------------------------
+	            
+	            JLabel iconLabel = new JLabel(fileIcon);
+	            //Uncomment below if running terrible code
+	            //JLabel iconLabel = new JLabel(resizedIcon);
+	            screen1.add(iconLabel);
+	            
 	            selectButton.setText("Select another");
+	            switchToScreen2.setBackground(buttonColor);
 	            fileLabel.setText("Selected file: " + file.getName() + ".");
 	        //----------------------------------------------------------
 	            
 	        //----------------GENERATE BUTTON STUFF--------------------
-	            if (!frame.getContentPane().isAncestorOf(panelBodyLower)) {
+	            if (!screen2.isAncestorOf(generateButton)) {
                     // Add panelBodyLower to the frame only if it's not already added
-                    panelBodyLower.add(generateButton);
-                    frame.add(panelBodyLower);
+                    screen2.add(generateButton);
+                    //frame.add(screen1);
                 }
 	        //---------------------------------------------------------
 	            //Resizes window to fit label/new buttons if needed
@@ -149,105 +217,23 @@ public class GUI implements ActionListener{
 		   File file = fc.getSelectedFile();
            String filePath = file.getAbsolutePath();
            
-
-           // where sample pdf is located
-           String inputFileName = filePath;
-
-           // Where results of pdf extraction will be output
-           String outputFolder = projectRoot += "/pdfextraction/src";
-
-           // Create a new folder named "output" to store images and result.txt
-           File folder = new File(outputFolder + "/output");
-           folder.mkdirs();
-           outputFolder = folder.getAbsolutePath(); // Update outputFolder with the new folder path
-
-           // create a TextExtraction Object
-           TextExtraction pdfExtractor = new TextExtraction(outputFolder);
-			try {
-				document = PDDocument.load(new File(inputFileName));
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-
-           // extract Images & Text from the Document
-           try {
-			pdfExtractor.runExtraction(document, projectRoot);
-           } catch (IOException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-           }
-
-           // Read the JSON file
-           @SuppressWarnings("deprecation")
-   			JsonParser parser = new JsonParser();
-           @SuppressWarnings("deprecation")
-           JsonObject jsonObject = null;
-			try {
-				jsonObject = parser.parse(new FileReader(projectRoot + "/output/parsedPDF.json")).getAsJsonObject();
-			} catch (JsonIOException e2) {
-				// TODO Auto-generated catch block
-				e2.printStackTrace();
-			} catch (JsonSyntaxException e2) {
-				// TODO Auto-generated catch block
-				e2.printStackTrace();
-			} catch (FileNotFoundException e2) {
-				// TODO Auto-generated catch block
-				e2.printStackTrace();
-			}
-
-           // Extract the content from the "text" field
-           String extractedText = jsonObject.get("text").getAsString();
-
-           // parse text and break up into paragraphs and store in a json
-	        try {
-				presentation = TextSegmenter.divide(extractedText);
-			} catch (JsonProcessingException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (InterruptedException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-
-           // summarize the text
-           try {
-			TextSummarizer.summarize(presentation);
-			} catch (InterruptedException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-
-           // generate powerpoint
-           
-
-           System.exit(0);
-           
 		   //----------------DOWNLOAD BUTTON STUFF--------------------
-		   panelBodyLower.add(Box.createRigidArea(new Dimension(0, 5))); // Vertical spacing of 10 pixels 
-		   panelBodyLower.add(downloadButton);
+		   screen2.add(Box.createRigidArea(new Dimension(0, 5))); // Vertical spacing of 10 pixels 
+		   screen2.add(downloadButton);
 		   //---------------------------------------------------------
 		   frame.pack();
 	   }
 	   if(e.getSource() == downloadButton) {
 		   //Add functionality
 		   frame.pack();
-		   try {
-				PowerPointGenerator.create(presentation, projectRoot);
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-
-	           try {
-				document.close();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+	   
 	   }
-	}
-	
-	
-
+    	if (e.getSource() == switchToScreen1) {
+    		cardLayout.show(cardPanel, "SCREEN1");
+    	}
+    	if (e.getSource() == switchToScreen2) {
+    		cardLayout.show(cardPanel, "SCREEN2");
+    	}
+    }
+	    	
 }
